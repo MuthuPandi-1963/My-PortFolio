493150ee5fb75f686e2fdfc3137562489de11fc2
__mr.prank <128305368+MuthuPandi-1963@users.noreply.github.com>
2024-12-05 16:31:02 +0530

    complete

--- src/components/ColorTheme/Theme.jsx
+++ src/components/ColorTheme/Theme.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import ThemeContext from "./themeContext";
+
 export default function Theme({children}) {
     const [theme,setTheme] = useState(false);
     const darkTheme = "pb-10 top-0 z-[-2] text-zinc-50 h-[100%] w-[100%] bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]";
