ddae65ca647f10cec5a87ab622a55553e0ad520a
__mr.prank <128305368+MuthuPandi-1963@users.noreply.github.com>
2024-12-06 14:47:01 +0530

    all done

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
-
 /** @type {import('tailwindcss').Config} */
+import tailwindScrollbar from 'tailwind-scrollbar';
+
 export default {
   content: [
     "./index.html",
@@ -10,20 +11,19 @@ export default {
       textShadow: {
         DEFAULT: '2px 2px rgba(0, 0, 0, 0.25)', // Default shadow
         sm: '1px 1px rgba(0, 0, 0, 0.1)',       // Small shadow
-        lg: '4px 4px rgba(0, 0, 0, 0.35)',       // Large shadow
+        lg: '4px 4px rgba(0, 0, 0, 0.35)',      // Large shadow
       },
       animation: {
         'custom-ping': 'ping 2s cubic-bezier(0, 0, 0.2, 1) infinite',
-      },
-      animation: {
         'slow-ping': 'ping 3s cubic-bezier(0, 0, 0.2, 1) infinite',
       },
-      boxShadow:{
-        'custom-lg':'inset 0 2px 4px blue,0 4px 6px rgba(0, 0, 0, 0.3)',
-      }
+      boxShadow: {
+        'custom-lg': 'inset 0 2px 4px blue, 0 4px 6px rgba(0, 0, 0, 0.3)',
+      },
     },
   },
   plugins: [
+    tailwindScrollbar, // Add the tailwind-scrollbar plugin
     function ({ addUtilities }) {
       addUtilities({
         '.text-shadow': {
@@ -41,19 +41,13 @@ export default {
         '.text-shadow-2xl': {
           textShadow: '12px 12px rgba(0, 0, 0, 0.35)',
         },
-        '.box-shadow-lg-light': { // Corrected class name
-        boxShadow: 'inset 0 0  25px 5px lightblue,5px 10px 10px 2px gray',
-      },
-      '.box-shadow-lg-dark': { // Corrected class name
-        boxShadow: 'inset 0 0  100px 0px #1f1f1f,2px 2px 10px blueviolet',
-      },
-      animation: {
-        'slow-ping': 'ping 2s cubic-bezier(0, 0, 0.2, 1) infinite',
-      },
+        '.box-shadow-lg-light': {
+          boxShadow: 'inset 0 0 25px 5px lightblue, 5px 10px 10px 2px gray',
+        },
+        '.box-shadow-lg-dark': {
+          boxShadow: 'inset 0 0 100px 0px #1f1f1f, 2px 2px 10px blueviolet',
+        },
       });
     },
-    
-      require('tailwind-scrollbar'), // Add the tailwind-scrollbar plugin
   ],
-}
-
+};
