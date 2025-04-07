d9dca9a5d41f0d316016d255b98da03f22739e0f
Muthupandi-1963 <muthupandir74738@gmail.com>
2025-04-07 11:00:59 +0530

    done

--- src/components/NavBar/MenuIcons.jsx
+++ src/components/NavBar/MenuIcons.jsx
@@ -1,20 +1,26 @@
 import { useContext } from "react";
 import ThemeContext from "../ColorTheme/themeContext";
-
+import { SiLeetcode } from "react-icons/si";
 import {DarkMode, LightMode ,Instagram , GitHub ,LinkedIn } from '@mui/icons-material';
 const MenuIconsItems = [
   {
+    icon :<SiLeetcode className="text-xl"/> ,
+    link : "https://leetcode.com/u/mr_prank__/"
+  },
+  {
     icon :<GitHub className="text-[10px]"/> ,
     link : "https://github.com/MuthuPandi-1963"
+  },
+  
+  {
+    icon :<LinkedIn className="text-[10px]"/> ,
+    link : "https://www.linkedin.com/in/muthupandi-r-832559325/"
   },
   {
     icon :<Instagram className="text-[10px]"/> ,
     link : "https://www.instagram.com/__mr.prank/profilecard/?igsh=cmhxeXdqNmV5NzVl"
   },
-  {
-    icon :<LinkedIn className="text-[10px]"/> ,
-    link : "https://www.linkedin.com/in/muthupandi-r-832559325/"
-  },
+  
 ]
 
 export default function MenuIcons() {
@@ -34,10 +40,10 @@ export default function MenuIcons() {
             <LightMode className="text-[10px]"/>
           </button>
         )}
-        <div className="">
+        <div className="flex items-center">
           {
           MenuIconsItems.map(({icon,link},id)=>(
-            <a href={link} key={id}>
+            <a href={link} key={id} className="">
           <button className={`${theme ? 'hover:text-blue-700' : 'hover:text-violet-800'}`}>
             {icon}
           </button>
